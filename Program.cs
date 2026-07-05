53fa5844665256f61754bcad50e2faf2b7436fd7
Nibin <bvarghesenibin@gmail.com>
2026-07-05 11:42:48 +0400

    fix local run and docker setup

--- Program.cs
+++ Program.cs
@@ -32,6 +32,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
+
 builder.Services.AddControllersWithViews();
 
 // SQLite DB
